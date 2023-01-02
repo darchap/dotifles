2af40bc73bfc5d0440403e6ed3b9a8c4f8fdef86
darchap <113114289+darchap@users.noreply.github.com>
2023-01-02 14:38:39 +0100

    Added upgrade function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,9 +1,20 @@
-# Colormap
-function colormap() {
-  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
-}
 
-function backupfile(){
+function backupfile() {
   cp $1 $1.bak
 }
 
+function upgrade() {
+  if [ "$(command -v nala)" ]; then
+    sudo nala upgrade -y
+  elif [ "$(command -v apt)" ]; then
+    sudo apt update && sudo apt full-upgrade --auto-remove -y
+  elif [ "$(command -v dnf)" ]; then
+    sudo dnf upgrade --best --allowerasing --refresh -y
+  elif [ "$(command -v flatpak)" ]; then
+    flatpak upgrade -y
+  fi
+}
+
+function colormap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+}
