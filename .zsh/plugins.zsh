e59b77ed73734996a6dd3ab0412b7d6446960be2
darchap <113114289+darchap@users.noreply.github.com>
2023-01-03 11:39:48 +0100

    Added option to automatically download zsh plugins

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,4 +1,10 @@
 # Use autosuggestion
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+fi
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 # Use syntax highlighting
+if [ ! -f "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+fi
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
