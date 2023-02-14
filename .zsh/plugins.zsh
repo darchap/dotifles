722a7d4ce772c2231135db6632ee5118ee7fc940
darchap <113114289+darchap@users.noreply.github.com>
2023-02-14 19:53:40 +0100

    Added zsh-ssh plugin

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -8,3 +8,8 @@ if [ ! -f "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; th
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 fi
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Use zsh-ssh
+if [ ! -f "$HOME/.zsh/zsh-ssh/zsh-ssh.zsh" ]; then
+    git clone https://github.com/sunlei/zsh-ssh ~/.zsh/zsh-ssh
+fi
+source ~/.zsh/zsh-ssh/zsh-ssh.zsh
